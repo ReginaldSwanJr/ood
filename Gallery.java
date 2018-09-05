ab1f58f8ae674486f672032615eab911411dc9ce
ReginaldSwanJr <swan2011@gmail.com>
2018-09-04 23:17:34 -0400

    added the hash table and changed two methods

--- Gallery.java
+++ Gallery.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.HashMap;
 
 /**
  * Write a description of class Gallery here.
@@ -11,6 +12,7 @@ import java.util.HashSet;
 public class Gallery implements CreationContainer
 {
     private ArrayList<Creation> creationList;
+    private HashMap <Creator,List<Creation>> myCreations;
     //private HashSet<CREATION_TYPE> typeList;
 
     /**
@@ -19,18 +21,41 @@ public class Gallery implements CreationContainer
     public Gallery()
     {
         creationList = new ArrayList<Creation>();
+        myCreations = new HashMap<>();
         //typeList = new HashSet<CREATION_TYPE>();
     }
 
     public boolean add(Creation creation)
     {
-        return creationList.add(creation);
+        //return creationList.add(creation);
+        Creator jim = creation.getCreator();
+        ArrayList<Creation> pam = new ArrayList<Creation>();
+        if(myCreations.containsKey(jim))
+        {
+            return myCreations.get(jim).add(creation);
+        }
+        else
+        {   
+            pam.add(creation);
+            myCreations.put(jim,pam);
+            return false;
+        }
     }
+    
 
     public Creation remove(Creation creation)
     {
-        boolean result = creationList.remove(creation);
-        return result ? creation : null;
+        //boolean result = creationList.remove(creation);
+        //return result ? creation : null;
+        if(myCreations.containsKey(creation.getCreator()))
+        {
+            myCreations.remove(creation.getCreator());
+            return creation;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     public int count()
@@ -60,6 +85,7 @@ public class Gallery implements CreationContainer
 
     public List<Creation> creationsByCreator(Creator creator)
     {
+        /* list to be returned
         List<Creation> creatorList = new ArrayList<Creation>();
         for(Creation creation : creationList)
         {
@@ -69,6 +95,9 @@ public class Gallery implements CreationContainer
             }
         }
         return creatorList;
+        */
+       
+        return myCreations.get(creator);
     }
 
     public List<Creation> creationsByType(CREATION_TYPE type)
